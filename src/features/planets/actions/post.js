8b81131bf7df646fdb00865ca86c72f6f34ef87c
Alexander Santos <alexander.diniz@g.globo>
2021-06-11 18:25:50 -0300

    fix: planet create for non-SWAPI planets

--- src/features/planets/actions/post.js
+++ src/features/planets/actions/post.js
@@ -1,19 +1,20 @@
 module.exports = async (req, res) => {
-  // TODO: CQS
-  const { SWAPI, planetsRepository, planetsValidations } = req.container;
+  try {
+    // TODO: CQS
+    const { SWAPI, planetsRepository, planetsValidations } = req.container;
 
-  const { error } = planetsValidations.planetCreateValidation.validate(
-    req.body,
-  );
-  if (error) return res.reply(400, 'Planeta inválido.', req.body, error);
+    const { error } = planetsValidations.planetCreateValidation.validate(
+      req.body,
+    );
+    if (error) return res.reply(400, 'Planeta inválido.', req.body, error);
 
-  const planet = req.body;
-  const searchRes = (await SWAPI.planets.search(planet.name)).data;
-  if (searchRes.count) {
-    planet.filmsCount = searchRes.results[0].films.length;
+    const planet = req.body;
+    const searchRes = (await SWAPI.planets.search(planet.name)).data;
+    planet.filmsCount = searchRes.count ? searchRes.results[0].films.length : 0;
     const createdPlanet = await planetsRepository.save(planet);
 
     return res.reply(200, 'Planeta salvo com sucesso.', createdPlanet);
+  } catch (err) {
+    return res.reply(500, 'Ocorreu um erro desconhecido ao salvar planeta.');
   }
-  return res.reply(500, 'Ocorreu um erro desconhecido ao salvar planeta.');
 };
